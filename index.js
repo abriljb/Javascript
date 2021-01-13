d55a8490d0991d85ebc34c290fb088864f572689
Abriljb <abriljb@yahoo.com>
2021-01-13 19:42:38 +0800

    Responsive Navigator for my future work :D Trust your self jb. Don't give up!

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const OpenChanger = document.querySelector('#iconTrigger')
-const CloseChanger = document.querySelector('#icon')
-const MenuContainer = document.querySelector('')
+const iconTrigger = document.querySelector('#iconTrigger')
+const icon = document.querySelector('#icon')
+const MenuContainer = document.querySelector('.Navigators')
+
+iconTrigger.addEventListener('click', () =>{
+    if(icon.classList.contains('fa-bars')){
+
+        icon.classList.remove('fa-bars')
+        icon.classList.add('fa-close')
+
+        // Navigator
+        MenuContainer.classList.remove('hide')
+        MenuContainer.classList.add('show')
+
+    }else{
+        icon.classList.add('fa-bars')
+        icon.classList.remove('fa-close')
+
+        // Navigator
+       MenuContainer.classList.remove('show')
+       MenuContainer.classList.add('hide')
+
+    }
+
+})
