3b9b49c98cd1f898d33cbc910254a365ff9d0097
john benedick abril <abriljb@yahoo.com>
2021-01-14 13:57:32 +0800

    JS PRACTICE

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-const OpenChanger = document.querySelector('#iconTrigger')
+const OpenChanger = document.querySelector('.icon-container')
 const CloseChanger = document.querySelector('#icon')
-const MenuContainer = document.querySelector('')
+const MenuContainer = document.querySelector('.hide')
+const para = document.querySelector('main')
+
+CloseChanger.addEventListener('click', () =>{
+    if(CloseChanger.classList.contains('fa-bars')){
+        CloseChanger.classList.remove('fa-bars')
+        CloseChanger.classList.add('fa-close')
+        MenuContainer.classList.remove('hide')
+        MenuContainer.classList.add('show')
+    }else{
+        CloseChanger.classList.add('fa-bars')
+        CloseChanger.classList.remove('fa-close')
+        MenuContainer.classList.add('hide')
+        MenuContainer.classList.remove ('show')
+    }
+    
+})
+
+const OpenClose = () =>{
+    
+    document.querySelector('#door').src="img/Opendoor.png";
+    document.querySelector('#statusdoor').innerHTML="This is a Opendoor";
+}
+
+const CloseOpen = () =>{
+    document.querySelector('#door').src="img/Closedoor.png";
+    document.querySelector('#statusdoor').innerHTML="This is a Closedoor";
+
+}
+const numberuno = document.querySelector('#numone')
+const numberpangalawa = document.querySelector('#numtwo')
+let pare = 0
+
+
+const yow = () =>{
+    
+   pare = Number(numberuno.value) + Number(numberpangalawa.value) 
+
+   alert(pare)
+}
+
+
+
